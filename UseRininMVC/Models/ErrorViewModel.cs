9024455515e51f450604c697f68db371c1bc6b31
abdohosny1 <make.y90@yahoo.com>
2024-01-31 17:00:58 +0200

    Add project files.

---
+++ UseRininMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UseRininMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ UseRininMVC/Program.cs
@@ -0,0 +1,38 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Logging.AddRinLogger();
+builder.Services.AddRin();
+
+builder.Services.AddControllersWithViews().AddRinMvcSupport();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+else
+{
+    app.UseRin();
+    app.UseDeveloperExceptionPage();
+    app.UseRinDiagnosticsHandler();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
